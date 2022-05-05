9604758b0d6a7cc7a96b10c11a55ec1c5b3776fc
nick wang <wenkun_wang@163.com>
2022-05-05 15:50:13 +0800

    初始化SpringBoot项目

---
+++ src/main/java/com/nick/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.nick.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
+public class HelloController {
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return "Hello There!";
+    }
+}
