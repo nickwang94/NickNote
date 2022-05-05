435e54874bb64454b3662686ca12bf3b675f2229
nick <wenkun_wang@163.com>
2022-05-05 17:40:49 +0800

    测试Jenkins自动拉取代码2

--- src/main/java/com/nick/controller/HelloController.java
+++ src/main/java/com/nick/controller/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String sayHello() {
-        return "Hello Msg From Jenkins!";
+        return "Hello Msg From Jenkins! Test2";
     }
 }
